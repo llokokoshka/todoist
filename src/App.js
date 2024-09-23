46951a8db4a2f516d294437be32ee2b7c4ee8e98
Katerina <katelokota@gmail.com>
2024-09-23 18:09:22 +0300

    From input in todo, but not correct

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
-function App() {
+let numberOfItems = 2;
+
+export default function App() {
+ const [todo, setTodo] = useState('');
+
+  function takeTodo(e){
+    setTodo( 
+      e.target.value,);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
+    <Body>
+      <Container>
+        <Title>
           todos
-        </h1>
-        <div>
-          <input type='text' placeholder='What needs to be done?'></input>
-        </div>
-        <div>
+        </Title>
+        <InputField>
+          <input type='text' value={todo} onChange={takeTodo} placeholder='What needs to be done?'></input>
+        </InputField>
+        <TodoBody>
 
-        </div>
-      </header>
-    </div>
+          <input type="checkbox"></input>
+          <div>{todo}</div>
+          <button>X</button>
+      </TodoBody>
+        <FooterBody>
+          <div>
+            {numberOfItems} items left
+          </div>
+          <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+          <div>
+            <button>Clear completed</button>
+          </div>
+        </FooterBody>
+      </Container>
+    </Body>
   );
 }
 
-export default App;
+const Body = styled.body`
+  * {
+    margin: 0 auto;
+    padding: 0;
+    border: 0;
+  }
+
+  background-color: #f5f5f5;
+	font-family: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	line-height: 1.4em;
+	color: #111111;
+	font-weight: 300;
+`
+
+const Container = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding: 0;
+  justify-content: space-between;
+  flex-direction: column;
+  max-width: 550px;
+`
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  color: #b83f45;
+`
+
+const InputField = styled.div`
+  display: flex;
+  justify-content: center;
+  input{
+    width: 100%;
+  } 
+`
+const TodoBody = styled.div`
+    display: flex;
+    margin: 0 auto;
+    padding: 0;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+`
+
+const FooterBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`
---
+++ src/components/todo.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Todo(){
+    return(
+        <TodoBody>
+            <input type="checkbox"></input>
+            <div></div>
+            <button>X</button>
+        </TodoBody>
+    )
+
+}
+
