d8b03961a3e048ba005b4637ee5f093eedc8622c
Katerina <katelokota@gmail.com>
2024-09-23 15:59:57 +0300

    deleted default files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          todos
+        </h1>
+        <div>
+          <input type='text' placeholder='What needs to be done?'></input>
+        </div>
+        <div>
+
+        </div>
       </header>
     </div>
   );
